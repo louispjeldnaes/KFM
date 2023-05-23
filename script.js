3cc5e03b99242ba68d1f93b0781728da8f31ce0e
KatrineLunding <katrine.brun.lunding@hotmail.dk>
2023-05-23 20:15:24 +0200

    Burger toggle funktionen implementeret (html, css og JS)

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+/* BURGER TOGGLE FUNKTION med CSS ANIMATION */
+
+/*Har lært om følgende properties og måde at opstille CSS-animationer på i
+Val Head's LinkedIn Learning kursus CSS Animation: https://www.linkedin.com/learning/css-animation-14153685?u=68595258
+ og på w3schools - har selv rettet dem til JS-syntax*/
+
+/*Forkortelser ved brug af const, fordi de er konstante*/
+const burgerBog = document.getElementById("burgerBog");
+const bogUnder = document.getElementById("bogUnder");
+const bogVenstre = document.getElementById("bogVenstre");
+const bogHoejre = document.getElementById("bogHoejre");
+
+const burgerIkon = document.getElementById("burgerIkon");
+const overlayNav = document.getElementById("overlayNav");
+
+let synlig = false;
+
+/*Burgerfunktionen sat op med if og else for at
+#burgerIkonCirkel kan bruges som toggle til både at
+åbne og lukke burgermenunen og viser CSS-animation
+i overgangen*/
+function burgerFunktion(){
+    if (synlig == false){
+        /* Åbner burgermenuen */
+        overlayNav.style.width = "100%";
+
+        /* sætter CSS-animationerne i gang */
+        burgerBog.style.animationPlayState = "running";
+        bogUnder.style.animationPlayState = "running";
+        bogVenstre.style.animationPlayState = "running";
+        bogHoejre.style.animationPlayState = "running";
+
+        
+        burgerBog.style.animationDirection = "normal"; 
+        bogUnder.style.animationDirection = "normal"; 
+        bogVenstre.style.animationDirection = "normal"; 
+        bogHoejre.style.animationDirection = "normal"; 
+
+        synlig = true;
+
+    }else{
+        /* Lukker burgermenuen */
+        overlayNav.style.width = "0%";
+
+        /* Siger til CSS-animationer at de skal afspilles baglæns,
+        altså fra 100% til 0%*/
+        burgerBog.style.animationDirection = "reverse"; 
+        bogUnder.style.animationDirection = "reverse"; 
+        bogVenstre.style.animationDirection = "reverse"; 
+        bogHoejre.style.animationDirection = "reverse";
+
+        synlig = false;
+    }    
+  }
+
+  /* Kalder funktionen (åben eller luk) ved
+  klik på div'en #burgerIkonCirkel */
+  burgerIkon.addEventListener("click", burgerFunktion);
+
+  
+
 //BOGKARRUSEL
 
 const venstrePil = document.getElementById("karruselHoejrePil");
@@ -13,7 +74,6 @@ const vistBog = document.getElementById("rulEnBogMedKnap");
 const rulEnBog = document.getElementById("rulEnBogKnap");
 
 /*Vi bruger Math.random til at generere en tilfældig bog*/
-
 function rul() {
   let resultat = Math.floor(Math.random() * 8) + 1;
   switch (resultat) {
@@ -55,24 +115,10 @@ function rul() {
 
 rulEnBog.addEventListener("click", rul);
 
-/* Åbner burgermenuen */
-function openNav() {
-  document.getElementById("overlayNav").style.width = "100%";
-  document.getElementById("overlay_burgermenu").style.display = "none";
-  document.getElementById("burgerCirkel").style.display = "none";
-}
 
-/* Lukker burgermenuen */
 
-function closeNav() {
-  document.getElementById("overlayNav").style.width = "0%";
-  document.getElementById("overlay_burgermenu").style.display = "block";
-  document.getElementById("burgerCirkelLuk").style.display = "block";
-}
 
 /* Fylder hjertet */
-
-
 function hjerteFyldes() {
   document.getElementById("hjerteTomt").style.display = "none";
   document.getElementById("hjerteFyldt").style.display = "block";
