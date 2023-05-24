e3a64a8bc29eb587b1f55816c8f84dc0dbd4898d
KatrineLunding <katrine.brun.lunding@hotmail.dk>
2023-05-24 16:36:32 +0200

    debugging af Event, læseklubber, head og footer

--- burgerscript.js
+++ burgerscript.js
@@ -57,69 +57,6 @@ function burgerFunktion(){
   klik på div'en #burgerIkonCirkel */
   burgerIkon.addEventListener("click", burgerFunktion);
 
-
-
-/*Funktion der starter afspilningen af CSS-animationer i burgeren
-function burgerAaben (){
-    burgerBog.style.animationPlayState = "running";
-    bogUnder.style.animationPlayState = "running";
-    bogVenstre.style.animationPlayState = "running";
-    bogHoejre.style.animationPlayState = "running";
-}
-
-/*Kalder burgerAaben-funktionen når der klikkes på div'en #burgerBog*/
-//burgerBog.addEventListener("click", burgerAaben);
-
-
-/*Funktion der starter afspilningen af CSS-animationer i burgeren
-og afspiller dem baglæns, så bog lukker igen
-function burgerLuk (){
-    /*animationFillMode backwards bruges så animationen afspiller
-    fra 100%-0% og stopper på sin slutposition og ikke
-    går tilbage til udgangspunktet
-    burgerBog.style.animationFillMode = "backwards"; 
-    bogUnder.style.animationFillMode = "backwards"; 
-    bogVenstre.style.animationFillMode = "backwards"; 
-    bogHoejre.style.animationFillMode = "backwards"; 
-
-    burgerBog.style.animationPlayState = "running";
-    bogUnder.style.animationPlayState = "running";
-    bogVenstre.style.animationPlayState = "running";
-    bogHoejre.style.animationPlayState = "running";
-} */
-
-/*Kalder burgerLuk-funktionen når der klikkes på div'en #burgerBog*/
-//burgerBog.addEventListener("click", burgerLuk);
-
-/* Åbner burgermenuen
-function aabenNav() {
-    overlayNav.style.width = "100%";
-
-    burgerBog.style.animationPlayState = "running";
-    bogUnder.style.animationPlayState = "running";
-    bogVenstre.style.animationPlayState = "running";
-    bogHoejre.style.animationPlayState = "running";
-  }
-
-  burgerIkonCirkel.addEventListener("click", aabenNav); */
-
-  /* Lukker burgermenuen
-  function lukNav() {
-    overlayNav.style.width = "0%";
-
-    burgerBog.style.animationFillMode = "backwards"; 
-    bogUnder.style.animationFillMode = "backwards"; 
-    bogVenstre.style.animationFillMode = "backwards"; 
-    bogHoejre.style.animationFillMode = "backwards"; 
-
-    burgerBog.style.animationPlayState = "running";
-    bogUnder.style.animationPlayState = "running";
-    bogVenstre.style.animationPlayState = "running";
-    bogHoejre.style.animationPlayState = "running";
-  }*/
-
-  //burgerIkonCirkel.addEventListener("click", lukNav);
-
  
 
 
