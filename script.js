30e8435f9b88ef40054060997aa84c503ea818fe
RexxMK <rikkekodahl@live.dk>
2023-05-24 10:41:50 +0200

    Bogkarrusel responsive

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- //BOGKARRUSEL
+//BOGKARRUSEL
 
 const venstrePil = document.getElementById("karruselHoejrePil");
 const hoejrePil = document.getElementById("karruselVenstrePil");
@@ -7,7 +7,6 @@ const hoejrePil = document.getElementById("karruselVenstrePil");
   const bogIKarruselWidth = bogIKarrusel.clientWidth;
   bogKarrusel.scrollLeft += bogIKarruselWidth;
 });*/
-
 
 //RUL EN BOG
 const vistBog = document.getElementById("rulEnBogMedKnap");
@@ -55,24 +54,19 @@ function rul() {
 
 rulEnBog.addEventListener("click", rul);
 
-
-
-
 /* Fylder hjertet */
 function hjerteFyldes() {
   document.getElementById("hjerteTomt").style.display = "none";
   document.getElementById("hjerteFyldt").style.display = "block";
-
 }
 
 /* Tømmer hjertet */
-function hjerteToemmes() {  
+function hjerteToemmes() {
   document.getElementById("hjerteFyldt").style.display = "none";
   document.getElementById("hjerteTomt").style.display = "block";
 }
 
-
-/*Åbner anmeldelse op*/ 
+/*Åbner anmeldelse op*/
 function aabenAnmeldelse() {
   document.getElementById("anmeldelseKort").style.display = "block";
 }
